968959f9a78ff085d49e2f3484645ea42c268b95
AlonePainPeanut <139093156+noturcmd@users.noreply.github.com>
2024-12-24 22:30:48 +0700

    p

--- ticketing_system/index.php
+++ ticketing_system/index.php
@@ -14,8 +14,9 @@ $gambarUser = getImageUser(getConnection(), $userID)[0]['image'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pemesanan Tiket</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <link rel="stylesheet" href="../styles/navbar.css">
 
     <link rel="stylesheet" href="../styles/style1.css">
